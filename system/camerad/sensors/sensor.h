626cfa199f55afbb407c92268ba8695df4863787
MagZu <magzu90@gmail.com>
2026-04-20 20:16:03 +0200

    Update sensor.h
    stuff probably not needed.

--- system/camerad/sensors/sensor.h
+++ system/camerad/sensors/sensor.h
@@ -112,4 +112,7 @@ public:
   std::vector<i2c_random_wr_payload> getExposureRegisters(int exposure_time, int new_exp_g, bool dc_gain_enabled) const override;
   float getExposureScore(float desired_ev, int exp_t, int exp_g_idx, float exp_gain, int gain_idx) const override;
   int getSlaveAddress(int port) const override;
+
+private:
+  mutable std::map<uint16_t, std::pair<int, int>> ar0231_register_lut;
 };
